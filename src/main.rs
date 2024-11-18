f3694271ed589975b81856e976df3218a6fd436b
Peter Hizalev <peter@hizalev.com>
2024-11-17 18:54:43 -0800

    Chapter 1, codegen

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use clap::Parser;
 use regex::Regex;
-use std::fs::read_to_string;
+use std::fs::{read_to_string, File};
+use std::io::Write;
 use std::path::PathBuf;
-use std::process::Command;
+use std::process::{Command, ExitStatus};
 use tempfile::tempdir;
 
 #[derive(Debug, Parser)]
@@ -36,21 +37,21 @@ enum Token<'a> {
 }
 
 #[derive(Debug)]
-enum Expression<'a> {
+enum CExpression<'a> {
     Constant(Constant<'a>),
 }
 #[derive(Debug)]
-enum Statement<'a> {
-    Return(Expression<'a>),
+enum CStatement<'a> {
+    Return(CExpression<'a>),
 }
 
 #[derive(Debug)]
-struct Function<'a> {
+struct CFunction<'a> {
     name: Identifier<'a>,
-    body: Statement<'a>,
+    body: CStatement<'a>,
 }
 #[derive(Debug)]
-struct Program<'a>(Function<'a>);
+struct CProgram<'a>(CFunction<'a>);
 
 fn lex(source: &String) -> Vec<Token> {
     let mut tokens = Vec::new();
@@ -86,7 +87,6 @@ fn lex(source: &String) -> Vec<Token> {
                 panic!()
             };
 
-            dbg!(&token);
             tokens.push(token);
 
             current = &current[end..];
@@ -99,19 +99,19 @@ fn lex(source: &String) -> Vec<Token> {
     tokens
 }
 
-fn parse_expression<'a>(tokens: &mut Vec<Token<'a>>) -> Expression<'a> {
+fn parse_expression<'a>(tokens: &mut Vec<Token<'a>>) -> CExpression<'a> {
     if let Some(Token::Constant(constant)) = tokens.pop() {
-        Expression::Constant(constant)
+        CExpression::Constant(constant)
     } else {
         panic!("Expected constant");
     }
 }
 
-fn parse_statement<'a>(tokens: &mut Vec<Token<'a>>) -> Statement<'a> {
+fn parse_statement<'a>(tokens: &mut Vec<Token<'a>>) -> CStatement<'a> {
     if let Some(Token::Return) = tokens.pop() {
         let expression = parse_expression(tokens);
         if let Some(Token::Semicolon) = tokens.pop() {
-            Statement::Return(expression)
+            CStatement::Return(expression)
         } else {
             panic!("Expected ;");
         }
@@ -120,14 +120,14 @@ fn parse_statement<'a>(tokens: &mut Vec<Token<'a>>) -> Statement<'a> {
     }
 }
 
-fn parse_function<'a>(tokens: &mut Vec<Token<'a>>) -> Function<'a> {
+fn parse_function<'a>(tokens: &mut Vec<Token<'a>>) -> CFunction<'a> {
     if let Some(Token::Int) = tokens.pop() {
         if let Some(Token::Identifier(identifier)) = tokens.pop() {
             if let Some(Token::OParen) = tokens.pop() {
                 if let Some(Token::Void) = tokens.pop() {
                     if let Some(Token::CParen) = tokens.pop() {
                         if let Some(Token::OBrace) = tokens.pop() {
-                            let function = Function {
+                            let function = CFunction {
                                 body: parse_statement(tokens),
                                 name: identifier,
                             };
@@ -157,13 +157,154 @@ fn parse_function<'a>(tokens: &mut Vec<Token<'a>>) -> Function<'a> {
     }
 }
 
-fn parse_program<'a>(tokens: &mut Vec<Token<'a>>) -> Program<'a> {
-    let program = Program(parse_function(tokens));
+fn parse_program<'a>(tokens: &mut Vec<Token<'a>>) -> CProgram<'a> {
+    let program = CProgram(parse_function(tokens));
 
     if tokens.is_empty() {
         program
     } else {
         panic!("Unexpected tokens at the end")
+    }
+}
+
+#[derive(Debug)]
+enum AsmRegister {
+    EAX,
+}
+
+#[derive(Debug)]
+enum AsmOperand<'a> {
+    Imm(Constant<'a>),
+    Register(AsmRegister),
+}
+
+#[derive(Debug)]
+enum AsmInstruction<'a> {
+    Move {
+        src: AsmOperand<'a>,
+        dst: AsmOperand<'a>,
+    },
+    Ret,
+}
+
+#[derive(Debug)]
+struct AsmFunction<'a> {
+    name: Identifier<'a>,
+    instructions: Vec<AsmInstruction<'a>>,
+}
+#[derive(Debug)]
+struct AsmProgram<'a>(AsmFunction<'a>);
+
+fn codegen_statement(statement: CStatement) -> Vec<AsmInstruction> {
+    let mut instructions = Vec::new();
+    match statement {
+        CStatement::Return(CExpression::Constant(constant)) => {
+            instructions.extend([
+                AsmInstruction::Move {
+                    src: AsmOperand::Imm(constant),
+                    dst: AsmOperand::Register(AsmRegister::EAX),
+                },
+                AsmInstruction::Ret,
+            ]);
+        }
+    }
+
+    instructions
+}
+
+fn codegen_function(function: CFunction) -> AsmFunction {
+    let CFunction { body, name } = function;
+    AsmFunction {
+        name,
+        instructions: codegen_statement(body),
+    }
+}
+
+fn codegen_program(program: CProgram) -> AsmProgram {
+    let CProgram(function) = program;
+    AsmProgram(codegen_function(function))
+}
+
+fn emit_register(register: AsmRegister) -> &'static str {
+    match register {
+        AsmRegister::EAX => "%eax",
+    }
+}
+
+fn emit_operand(operand: AsmOperand) -> Vec<&str> {
+    let mut text = Vec::new();
+    match operand {
+        AsmOperand::Imm(constant) => {
+            text.push("$");
+            text.push(constant.0);
+        }
+
+        AsmOperand::Register(register) => text.push(emit_register(register)),
+    }
+
+    text
+}
+
+fn emit_instruction(instruction: AsmInstruction) -> Vec<&str> {
+    let mut text = Vec::new();
+    text.push("\t");
+
+    match instruction {
+        AsmInstruction::Move { src, dst } => {
+            text.push("movl ");
+            text.extend(emit_operand(src));
+            text.push(", ");
+            text.extend(emit_operand(dst));
+            text.push("\n");
+        }
+        AsmInstruction::Ret => text.push("ret\n"),
+    }
+
+    text
+}
+
+fn emit_function(function: AsmFunction) -> Vec<&str> {
+    let AsmFunction { name, instructions } = function;
+    let mut text = Vec::new();
+
+    text.push("\t.globl ");
+    text.push(name.0);
+    text.push("\n");
+    text.push(name.0);
+    text.push(":\n");
+
+    for instruction in instructions {
+        text.extend(emit_instruction(instruction));
+    }
+
+    text
+}
+
+fn emit_program(program: AsmProgram) -> Vec<&str> {
+    let mut text = Vec::new();
+    let AsmProgram(function) = program;
+
+    text.extend(emit_function(function));
+    text.push("\n.section .note.GNU-stack,\"\",@progbits\n");
+
+    text
+}
+
+fn run_gcc<'a, I>(args: I) -> ExitStatus
+where
+    I: IntoIterator<Item = &'a str>,
+{
+    if let Ok(status) = Command::new("gcc").args(args).status() {
+        status
+    } else {
+        panic!("GCC not found")
+    }
+}
+
+fn write_asm_text(path: &PathBuf, text: Vec<&str>) {
+    let mut file = File::create(path).unwrap();
+    for s in text {
+        file.write(s.as_bytes()).unwrap();
     }
 }
 
@@ -174,34 +315,50 @@ fn main() {
     if let Ok(temp_dir) = tempdir() {
         let preprocessed_path = temp_dir.path().join(source_path.file_name().unwrap());
 
-        if let Ok(status) = Command::new("gcc")
-            .args([
-                "-E",
-                "-P",
-                source_path.to_str().unwrap(),
-                "-o",
-                preprocessed_path.to_str().unwrap(),
-            ])
-            .status()
+        if run_gcc([
+            "-E",
+            "-P",
+            source_path.to_str().unwrap(),
+            "-o",
+            preprocessed_path.to_str().unwrap(),
+        ])
+        .success()
         {
-            if status.success() {
-                match read_to_string(preprocessed_path) {
-                    Ok(source) => {
-                        let mut tokens = lex(&source);
-                        tokens.reverse();
-                        let program = parse_program(&mut tokens);
-                        dbg!(program);
+            match read_to_string(preprocessed_path) {
+                Ok(source) => {
+                    let mut tokens = lex(&source);
+                    dbg!(&tokens);
+                    tokens.reverse();
+                    let c_program = parse_program(&mut tokens);
+                    dbg!(&c_program);
+                    let asm_program = codegen_program(c_program);
+                    dbg!(&asm_program);
+                    let asm_text = emit_program(asm_program);
+                    dbg!(&asm_text);
+
+                    let asm_path = temp_dir
+                        .path()
+                        .join(source_path.file_stem().unwrap())
+                        .with_extension("s");
+
+                    write_asm_text(&asm_path, asm_text);
+
+                    let exe_path = source_path
+                        .parent()
+                        .unwrap()
+                        .join(source_path.file_stem().unwrap());
+
+                    if !run_gcc([asm_path.to_str().unwrap(), "-o", exe_path.to_str().unwrap()])
+                        .success()
+                    {
+                        panic!("Assembly compilation failed")
                     }
-                    Err(_) => panic!("Cannot read file"),
                 }
-            } else {
-                panic!("Preprocessing failed")
+                Err(_) => panic!("Cannot read preprocessed source"),
             }
         } else {
-            panic!("GCC not found")
+            panic!("Preprocessing failed")
         }
-
-        temp_dir.close().unwrap();
     } else {
         panic!("Error creating temp dir")
     }
