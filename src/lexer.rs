3ec819d35027ffb26699c2af036ce3ddaeeb4949
Peter Hizalev <peter@hizalev.com>
2024-12-05 20:54:28 -0800

    Chapter 4, lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,4 @@
-use regex::Regex;
+use regex::{Match, Regex};
 
 use crate::parser;
 
@@ -21,61 +21,246 @@ pub enum Token<'a> {
     Star,
     Slash,
     Percent,
+    LessThanEqual,
+    LessThan,
+    GreaterThanEqual,
+    GreaterThan,
+    DoubleEqual,
+    AsteriskEqual,
+    Asterisk,
+    DoubleAmpersand,
+    DoublePipe,
+}
+
+struct RegexTokenizer<'a, 'b> {
+    regex: &'a str,
+    match_to_token: fn(Match<'b>) -> Token<'b>,
 }
 
 pub fn lex(source: &String) -> Vec<Token> {
+    let mut regex_tokenizers: Vec<(&str, RegexTokenizer)> = Vec::new();
+
+    regex_tokenizers.push((
+        "int",
+        RegexTokenizer {
+            regex: "(?<int>int\\b)",
+            match_to_token: |_| Token::Int,
+        },
+    ));
+    regex_tokenizers.push((
+        "void",
+        RegexTokenizer {
+            regex: "(?<void>void\\b)",
+            match_to_token: |_| Token::Void,
+        },
+    ));
+    regex_tokenizers.push((
+        "return",
+        RegexTokenizer {
+            regex: "(?<return>return\\b)",
+            match_to_token: |_| Token::Return,
+        },
+    ));
+    regex_tokenizers.push((
+        "identifier",
+        RegexTokenizer {
+            regex: "(?<identifier>[a-zA-Z_]\\w*\\b)",
+            match_to_token: |m| Token::Identifier(parser::Identifier(m.as_str())),
+        },
+    ));
+    regex_tokenizers.push((
+        "constant",
+        RegexTokenizer {
+            regex: "(?<constant>[0-9]+\\b)",
+            match_to_token: |m| Token::Constant(parser::Constant(m.as_str())),
+        },
+    ));
+    regex_tokenizers.push((
+        "o_paren",
+        RegexTokenizer {
+            regex: "(?<o_paren>\\()",
+            match_to_token: |_| Token::OParen,
+        },
+    ));
+    regex_tokenizers.push((
+        "c_paren",
+        RegexTokenizer {
+            regex: "(?<c_paren>\\))",
+            match_to_token: |_| Token::CParen,
+        },
+    ));
+    regex_tokenizers.push((
+        "o_brace",
+        RegexTokenizer {
+            regex: "(?<o_brace>\\{)",
+            match_to_token: |_| Token::OBrace,
+        },
+    ));
+    regex_tokenizers.push((
+        "c_brace",
+        RegexTokenizer {
+            regex: "(?<c_brace>\\})",
+            match_to_token: |_| Token::CBrace,
+        },
+    ));
+    regex_tokenizers.push((
+        "semicolon",
+        RegexTokenizer {
+            regex: "(?<semicolon>;)",
+            match_to_token: |_| Token::Semicolon,
+        },
+    ));
+    regex_tokenizers.push((
+        "minus",
+        RegexTokenizer {
+            regex: "(?<minus>-)",
+            match_to_token: |_| Token::Minus,
+        },
+    ));
+    regex_tokenizers.push((
+        "double_minus",
+        RegexTokenizer {
+            regex: "(?<double_minus>--)",
+            match_to_token: |_| Token::DoubleMinus,
+        },
+    ));
+    regex_tokenizers.push((
+        "tilde",
+        RegexTokenizer {
+            regex: "(?<tilde>~)",
+            match_to_token: |_| Token::Tilde,
+        },
+    ));
+    regex_tokenizers.push((
+        "plus",
+        RegexTokenizer {
+            regex: "(?<plus>\\+)",
+            match_to_token: |_| Token::Plus,
+        },
+    ));
+    regex_tokenizers.push((
+        "star",
+        RegexTokenizer {
+            regex: "(?<star>\\*)",
+            match_to_token: |_| Token::Star,
+        },
+    ));
+    regex_tokenizers.push((
+        "slash",
+        RegexTokenizer {
+            regex: "(?<slash>/)",
+            match_to_token: |_| Token::Slash,
+        },
+    ));
+    regex_tokenizers.push((
+        "percent",
+        RegexTokenizer {
+            regex: "(?<percent>%)",
+            match_to_token: |_| Token::Percent,
+        },
+    ));
+
+    regex_tokenizers.push((
+        "less_than_equal",
+        RegexTokenizer {
+            regex: "(?<less_than_equal><=)",
+            match_to_token: |_| Token::LessThanEqual,
+        },
+    ));
+
+    regex_tokenizers.push((
+        "less_than",
+        RegexTokenizer {
+            regex: "(?<less_than><)",
+            match_to_token: |_| Token::LessThan,
+        },
+    ));
+
+    regex_tokenizers.push((
+        "greater_than_equal",
+        RegexTokenizer {
+            regex: "(?<greater_than_equal>>=)",
+            match_to_token: |_| Token::GreaterThanEqual,
+        },
+    ));
+
+    regex_tokenizers.push((
+        "greater_than",
+        RegexTokenizer {
+            regex: "(?<greater_than>>)",
+            match_to_token: |_| Token::GreaterThan,
+        },
+    ));
+
+    regex_tokenizers.push((
+        "double_equal",
+        RegexTokenizer {
+            regex: "(?<double_equal>==)",
+            match_to_token: |_| Token::DoubleEqual,
+        },
+    ));
+
+    regex_tokenizers.push((
+        "asterisk_equal",
+        RegexTokenizer {
+            regex: "(?<asterisk_equal>!=)",
+            match_to_token: |_| Token::AsteriskEqual,
+        },
+    ));
+
+    regex_tokenizers.push((
+        "asterisk",
+        RegexTokenizer {
+            regex: "(?<asterisk>!)",
+            match_to_token: |_| Token::Asterisk,
+        },
+    ));
+
+    regex_tokenizers.push((
+        "double_ampersand",
+        RegexTokenizer {
+            regex: "(?<double_ampersand>&&)",
+            match_to_token: |_| Token::DoubleAmpersand,
+        },
+    ));
+
+    regex_tokenizers.push((
+        "double_pipe",
+        RegexTokenizer {
+            regex: "(?<double_pipe>\\|\\|)",
+            match_to_token: |_| Token::DoublePipe,
+        },
+    ));
+
     let mut tokens = Vec::new();
     let mut current = &source[..];
     current = current.trim_start();
-    let re =
-        Regex::new("\\A(?:(?<int>int\\b)|(?<void>void\\b)|(?<return>return\\b)|(?<identifier>[a-zA-Z_]\\w*\\b)|(?<constant>[0-9]+\\b)|(?<o_paren>\\()|(?<c_paren>\\))|(?<o_brace>\\{)|(?<c_brace>\\})|(?<semicolon>;)|(?<minus>-)|(?<double_minus>--)|(?<tilde>~)|(?<plus>\\+)|(?<star>\\*)|(?<slash>/)|(?<percent>%))").unwrap();
+
+    let re = Regex::new(&format!(
+        "\\A(?:{})",
+        regex_tokenizers
+            .iter()
+            .map(|(_, rt)| rt.regex)
+            .collect::<Vec<_>>()
+            .join("|")
+    ))
+    .unwrap();
+
     while current.len() != 0 {
         let ca = re.captures(current);
 
         if let Some(ca) = ca {
-            let (token, end) = if let Some(m) = ca.name("int") {
-                (Token::Int, m.end())
-            } else if let Some(m) = ca.name("void") {
-                (Token::Void, m.end())
-            } else if let Some(m) = ca.name("return") {
-                (Token::Return, m.end())
-            } else if let Some(m) = ca.name("identifier") {
-                (Token::Identifier(parser::Identifier(m.as_str())), m.end())
-            } else if let Some(m) = ca.name("constant") {
-                (Token::Constant(parser::Constant(m.as_str())), m.end())
-            } else if let Some(m) = ca.name("o_paren") {
-                (Token::OParen, m.end())
-            } else if let Some(m) = ca.name("c_paren") {
-                (Token::CParen, m.end())
-            } else if let Some(m) = ca.name("o_brace") {
-                (Token::OBrace, m.end())
-            } else if let Some(m) = ca.name("c_brace") {
-                (Token::CBrace, m.end())
-            } else if let Some(m) = ca.name("semicolon") {
-                (Token::Semicolon, m.end())
-            } else if let Some(m) = ca.name("minus") {
-                (Token::Minus, m.end())
-            } else if let Some(m) = ca.name("double_minus") {
-                (Token::DoubleMinus, m.end())
-            } else if let Some(m) = ca.name("tilde") {
-                (Token::Tilde, m.end())
-            } else if let Some(m) = ca.name("plus") {
-                (Token::Plus, m.end())
-            } else if let Some(m) = ca.name("star") {
-                (Token::Star, m.end())
-            } else if let Some(m) = ca.name("slash") {
-                (Token::Slash, m.end())
-            } else if let Some(m) = ca.name("percent") {
-                (Token::Percent, m.end())
-            } else {
-                panic!()
-            };
+            for (name, rt) in regex_tokenizers.iter() {
+                if let Some(m) = ca.name(name) {
+                    tokens.push((rt.match_to_token)(m));
+                    current = &current[m.end()..];
+                    current = current.trim_start();
 
-            tokens.push(token);
-
-            current = &current[end..];
-            current = current.trim_start();
+                    break;
+                }
+            }
         } else {
+            println!("{}", current);
             panic!();
         }
     }
